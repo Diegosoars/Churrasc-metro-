cafd4511d2f27416b4fd94c57bbc0c6c8ab42614
Diego Soares <diegosoarss@gmail.com>
2022-03-11 01:47:49 -0300

    Alteração 2

---
+++ script/script.js
@@ -0,0 +1,9 @@
+// carne - 400 p/ pessoa + de 6 horas = 650 p/ pessoa 
+// cerveja - 1200 ml p/ pessoa + de 6 horas = 2000 ml /p pessoa 
+// Refrigerante - 1000 ml p/ pessoa + de 6 horas = 1500 ml /p pessoa 
+// Criança valem 0,5 
+
+let inputAdultos = document.getElementById ("adultos");
+let inputCrianças = document.getElementById ("criancas");
+let inputDuração = document.getElementById ("duracao");
+let resultado = document.getElementById ("resultado")
